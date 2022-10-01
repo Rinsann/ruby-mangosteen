aa12b8cde17f23acf060655b2ca4cfec92e9ae28
Rin <785879986@qq.com>
2022-10-01 10:49:01 +0800

    解决 validation_codes_controller 没有返回邮件错误的 bug

--- spec/requests/api/v1/validation_codes_spec.rb
+++ spec/requests/api/v1/validation_codes_spec.rb
@@ -3,10 +3,16 @@ require 'rails_helper'
 RSpec.describe "ValidationCodes", type: :request do
   describe "验证码" do
     it "发送太频繁就会返回 429" do
-      post '/api/v1/validation_codes', params: {email: 'zenxinwo@foxmail.com'}
+      post '/api/v1/validation_codes', params: {email: 'fangyinghang@foxmail.com'}
       expect(response).to have_http_status(200)
-      post '/api/v1/validation_codes', params: {email: 'zenxinwo@foxmail.com'}
+      post '/api/v1/validation_codes', params: {email: 'fangyinghang@foxmail.com'}
       expect(response).to have_http_status(429)
+    end
+    it "邮件不合法就返回 422" do
+      post '/api/v1/validation_codes', params: {email: '1'}
+      expect(response).to have_http_status(422)
+      json = JSON.parse response.body
+      expect(json['errors']['email'][0]).to eq('is invalid')
     end
   end
 end
